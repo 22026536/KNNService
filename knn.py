7940b26a8ee0156c3f18da59e6251b42cffcc690
22026536 <22026536>
2024-12-17 07:28:14 +0700

    'quang'

--- knn.py
+++ knn.py
@@ -124,4 +124,4 @@ async def recommend_by_user(request: Request):
 # Chạy server
 if __name__ == "__main__":
     port = int(os.getenv("PORT", 8000))  # Render sẽ cung cấp cổng trong biến PORT
-    uvicorn.run("main:app", host="0.0.0.0", port=port, reload=True)
+    uvicorn.run("knn:app", host="0.0.0.0", port=port, reload=True)
